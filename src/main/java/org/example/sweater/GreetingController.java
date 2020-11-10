f0e17eafbf11419997f88109ebaa337dec6d6c2a
magidin91 <magidin@mail.ru>
2020-11-10 18:33:14 +0500

    3. Spring Boot JPA (Hibernate): добавляем базу данных в веб приложение на Java

--- src/main/java/org/example/sweater/GreetingController.java
+++ src/main/java/org/example/sweater/GreetingController.java
@@ -1,9 +1,14 @@
 package org.example.sweater;
 
+import org.example.sweater.entity.Message;
+import org.example.sweater.repository.MessageRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -11,6 +16,12 @@ import java.util.Map;
  */
 @Controller
 public class GreetingController {
+    final MessageRepo messageRepo;
+
+    public GreetingController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name = "name", required = false, defaultValue = "World") String name,
@@ -18,5 +29,35 @@ public class GreetingController {
     ) {
         model.put("name", name);
         return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        /* добавили в модель теущие сообщения и снова вернули шаблон main*/
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String tag, Map<String, Object> model) {
+        List<Message> messages = new ArrayList<>();
+        if (tag != null && !tag.isBlank()) {
+            messages = messageRepo.findByTag(tag);
+        }
+
+        model.put("messages", messages);
+        return "main";
     }
 }
